7d83a39a9d3f3671b4e9919b75245449ec798105
Sergey Litvinov <slitvinov@gmail.com>
2024-01-11 06:20:00 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,24 @@
+<h2>Analysis of red cell cofocal images</h2>
+
+<h3>References</h3>
+
+Simionato, G., Hinkelmann, K., Chachanidze, R., Bianchi, P., Fermo,
+E., van Wijk, R., ... & Quint, S. (2021). Red blood cell phenotyping
+from 3D confocal images using artificial neural networks. PLoS
+computational biology, 17(5), e1008934.
+
+<https://github.com/kgh-85/cytoShapeNet>
+
+<h3>Install</h3>
+
+<pre>
+$ python -m pip install --no-deps tifffile
+</pre>
+
+<h3>Convert</h3>
+
+<pre>
+$ python tif2xdmf.py ref/000137.tif
+tiff2xdmf: 101x101x185le.000137.raw
+tiff2xdmf: 000137.xdmf2
+</pre>
---
+++ tif2xdmf.py
@@ -0,0 +1,67 @@
+import numpy as np
+import os
+import re
+import sys
+import tifffile
+
+for path in sys.argv[1:]:
+    basename = re.sub("\.tif$", "", os.path.basename(path))
+    try:
+        input = tifffile.TiffFile(path)
+    except (FileNotFoundError, tifffile.tifffile.TiffFileError) as e:
+        sys.stderr.write("tiff2xdmf: %s\n" % e)
+        sys.exit(1)
+    dtype = input.pages[0].dtype
+    if dtype != np.dtype("uint8"):
+        sys.stderr.write("tiff2xdmf: error: only dtype=uint8 is supported\n")
+        sys.exit(1)
+
+    nz = len(input.pages)
+    ny, nx = input.pages[0].shape
+    raw = "%dx%dx%dle.%s.raw" % (nx, ny, nz, basename)
+    xdmf = "%s.xdmf2" % basename
+    out = np.ndarray((nx, ny), dtype)
+    with open(raw, "wb") as f:
+        for page in input.pages:
+            page.asarray(out=out)
+            f.write(out.tobytes())
+    with open(xdmf, "w") as f:
+        f.write("""\
+    <Xdmf
+        Version="2">
+      <Domain>
+        <Grid>
+          <Topology
+              TopologyType="3DCoRectMesh"
+              Dimensions="%d %d %d"/>
+          <Geometry
+              GeometryType="ORIGIN_DXDYDZ">
+            <DataItem
+                Dimensions="3">
+              0
+              0
+              0
+            </DataItem>
+            <DataItem
+                Dimensions="3">
+              1
+              1
+              1
+            </DataItem>
+          </Geometry>
+          <Attribute
+              Name="u"
+              Center="Cell">
+            <DataItem
+                NumberType="UChar"
+                Format="Binary"
+                Dimensions="%ld %ld %ld">
+              %s
+            </DataItem>
+          </Attribute>
+        </Grid>
+      </Domain>
+    </Xdmf>
+    """ % (nz + 1, ny + 1, nx + 1, nz, ny, nx, raw))
+    sys.stderr.write("tiff2xdmf: %s\n" % raw)
+    sys.stderr.write("tiff2xdmf: %s\n" % xdmf)
